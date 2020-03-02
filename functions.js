35f6f158089e2cb19d2d4f15db963ed44df21f4f
michaellhobart <michaellhobart@gmail.com>
2020-03-02 13:31:34 -0500

    ADDED TODOIST CONNECTION
    Added axios and uuid
    Addec connecton to post tasks.

--- functions.js
+++ functions.js
@@ -1,5 +1,34 @@
+const axios = require('axios').default
+const uuidv4 = require('uuid/v4');
+
+const reqUuid = uuidv4();
+const tempId = uuidv4();
+
+const newProj = (sender, task) => {
+  axios({
+      method: 'post',
+      url: 'https://api.todoist.com/sync/v8/sync',
+      data : {
+        token: "<token>",
+        commands: [{
+          type: "item_add",
+          temp_id: uuidv4(),
+          uuid: uuidv4(),
+          args: {
+            content: `${task} | ${sender}`,
+            project_id: "<project id",
+          }
+        }]
+      }
+    })
+    .then((res) => {
+      console.log('rebst', res);
+    })
+}
+
+
 const submitTask = (event) => {
   const sender = document.getElementById("td-sender").value;
   const task = document.getElementById("td-task").value;
-  console.log(`${sender} sent the task ${task}`);
+  newProj(sender, task)
 }
