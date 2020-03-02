cf7086a4c03c75e2dc6b2d12c9e13533da42e99c
michaellhobart <michaellhobart@gmail.com>
2020-03-02 13:18:24 -0500

    ADDED TODOIST CONNECTION
    Added axios and uuid packages.
    Added connection to Todoist API
    Removed Token & Project ID

--- functions.js
+++ functions.js
@@ -1,5 +1,34 @@
+const axios = require('axios').default
+const uuidv4 = require('uuid/v4');
+
+const reqUuid = uuidv4();
+const tempId = uuidv4();
+
+const newProj = (sender, task) => {
+  axios({
+      method: 'post',
+      url: 'https://api.todoist.com/sync/v8/sync',
+      data : {
+        token: '9a0f6ecddbf1a5ff14f92db52c4e48a3a2cea00b',
+        commands: [{
+          type: "item_add",
+          temp_id: uuidv4(),
+          uuid: uuidv4(),
+          args: {
+            content: `${task} | ${sender}`,
+            project_id: "2214138921",
+          }
+        }]
+      }
+    })
+    .then((res) => {
+      console.log('rebst', res);
+    })
+}
+
+
 const submitTask = (event) => {
   const sender = document.getElementById("td-sender").value;
   const task = document.getElementById("td-task").value;
-  console.log(`${sender} sent the task ${task}`);
+  newProj(sender, task)
 }
